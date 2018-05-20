0a3a9105f91bda55d19bb9a1140bf40b9e11f147
chester <ckraigpc@gmail.com>
2018-05-19 23:49:32 -0700

    Template for weekend exercise for number guessing

---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+GAME FUNCTION
+- Player must guess a number between a min and max
+- Player gets a certain amount guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+
+ */
+
+    
+ //Set GAME values here
+
+
+
+
+// Set UI elements here as variable here
+
+
+
+// Assign UI min and max here
+
+
+
+// Listen for guess
+
+  /*- Validate
+    - Check if won
+      1. Disable input 
+      2. Change the border color
+      3. Clear the input
+      4. call a function name setMessage() here
+
+    - Check else if not won
+      1. Wrong Number
+
+      2. Check if guesses is 0
+        * Game over -  lost
+        * Set the value of button to "Play Again"
+      3. Game continues - answer wrong
+  */
+
+// Set the function setMessage here
+
+
+
+
+
+
+  
+
+
+
